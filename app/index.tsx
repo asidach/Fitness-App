bd820341fcf9bf28fd0faa2ae5c052f2f19880d3
asidach <asidach@seas.upenn.edu>
2025-02-18 17:57:51 -0500

    Login page elements

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Text, FlatList, TextInput } from "react-native";
+import { Text, StyleSheet, TextInput, Pressable } from "react-native";
 import axios from "axios";
 import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 
@@ -8,27 +8,53 @@ const LoginPage = () => {
   const [userOrEmail, setUserOrEmail] = useState(''); // user can enter their username or email address
   const [password, setPassword] = useState(''); // password associated with account
 
-  /*
-  Example code to call from MongoDB
-
   useEffect(() => {
     axios.get("http://localhost:5001/users") // Change to your server URL
-      .then(response => setUsers(response.data))
+      .then(response => setPassword(response.data))
       .catch(error => console.log(error));
   }, []);
-  */
+  
 
   return (
     <SafeAreaProvider>
       <SafeAreaView>
-        <Text>Users List</Text>
+        <Text>Username or Email</Text>
         <TextInput
+          placeholder="Username or Email"
+          style={styles.textInput}
           onChangeText={setUserOrEmail}
         />
+        <Text>Password</Text>
+        <TextInput
+          placeholder="Password"
+          style={styles.textInput}
+          onChangeText={setUserOrEmail}
+        />
+        <Pressable
+          style={styles.button}
+        >
+          <Text>Login</Text>
+        </Pressable>
+        <Text>Don't have an account? Create one here</Text>
       </SafeAreaView>
     </SafeAreaProvider>
   );
 };
 
-export default LoginPage;
+const styles = StyleSheet.create({
+  textInput: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5
+  },
+  button: {
+    height: 20,
+    backgroundColor: "blue",
+    margin: 20,
+    alignItems: "center"
+  }
+})
 
+export default LoginPage;
