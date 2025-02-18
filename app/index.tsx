081333de0d98f0e5a66382994ef63f7a5506a7cd
asidach <asidach@seas.upenn.edu>
2025-02-18 16:08:38 -0500

    Set up server

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+/*import { Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -12,4 +12,31 @@ export default function Index() {
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
-}
+}*/
+import React, { useEffect, useState } from "react";
+import { View, Text, FlatList } from "react-native";
+import axios from "axios";
+
+const App = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/users") // Change to your server URL
+      .then(response => setUsers(response.data))
+      .catch(error => console.log(error));
+  }, []);
+
+  return (
+    <View>
+      <Text>Users List</Text>
+      <FlatList
+        data={users}
+        keyExtractor={(item) => item._id}
+        renderItem={({ item }) => <Text>{item.name} - {item.email}</Text>}
+      />
+    </View>
+  );
+};
+
+export default App;
+
---
+++ server/server.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+// Create a sample model
+const User = mongoose.model("User", new mongoose.Schema({ name: String, email: String }));
+
+// API endpoint to get all users
+app.get("/users", async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+});
+
+// API endpoint to add a user
+app.post("/users", async (req, res) => {
+  const newUser = new User(req.body);
+  await newUser.save();
+  res.json(newUser);
+});
+
+// Start the server
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
