5b4f8cc7123c0f61c72286e6c9684b8080d50365
asidach <asidach@seas.upenn.edu>
2025-02-19 14:30:02 -0500

    Add required fields

--- app/CreateAccount.tsx
+++ app/CreateAccount.tsx
@@ -1,20 +1,35 @@
 import React, { useEffect, useState } from "react";
-import { Text, StyleSheet, TextInput, Pressable } from "react-native";
+import { Text, StyleSheet, TextInput, Pressable, NativeSyntheticEvent, TextInputEndEditingEventData } from "react-native";
 import axios from "axios";
 import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 
 const CreateAccount = () => {
 
   const [username, setUsername] = useState(''); // unique username
+  const [usernameBlank, setUsernameBlank] = useState(false); // check if user entered a username
+
   const [email, setEmail] = useState(''); // personal email of user, will be used for notifications
+  const [emailBlank, setEmailBlank] = useState(false); // check if user entered an email
+
   const [password, setPassword] = useState(''); // password for account security
   const [confirmPass, setConfirmPass] = useState(''); // compare to password to ensure it is entered correctly
 
-  useEffect(() => {
-    axios.get("http://localhost:5001/users") // Change to your server URL
-      .then(response => setPassword(response.data))
-      .catch(error => console.log(error));
-  }, []);
+  // check data validations on username
+  // if all good, push username to database
+  async function checkUsername(e) {
+
+    // username cannot be blank
+    setUsernameBlank(username.trim() === '');
+
+  }
+
+  // check data validations on email
+  // if all good, push email to database
+  async function checkEmail(e) {
+
+
+
+  }
   
 
   return (
@@ -25,7 +40,10 @@ const CreateAccount = () => {
           placeholder="Enter Username"
           style={styles.textInput}
           onChangeText={setUsername}
+          onBlur={(e) => { checkUsername(e) }}
+          value={username}
         />
+        {usernameBlank && <Text>Username cannot be blank</Text>}
         <Text>Email Address</Text>
         <TextInput
           placeholder="Enter Email"
